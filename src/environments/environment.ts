c2ad2254a5199ee7a1ed6a4a41b8e24f4ecf494d
Joao Pedro <joao-pedrofonseca22@hotmail.com>
2022-03-18 20:28:00 -0300

    adding firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDgaQ4DOYSDIGxompH1OFW49EOScF-byrg",
+    authDomain: "crud-hardware.firebaseapp.com",
+    projectId: "crud-hardware",
+    storageBucket: "crud-hardware.appspot.com",
+    messagingSenderId: "184385960381",
+    appId: "1:184385960381:web:1c223f7124978a9afc7ad4"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
